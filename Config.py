67ba9f3466728750e0ed0462f3aeb9ce53bd7023
ProstoVitya <79058360+ProstoVitya@users.noreply.github.com>
2022-12-07 15:52:58 +0400

    добавил фазовые кривые

--- Config.py
+++ Config.py
@@ -5,14 +5,12 @@ delta = 1
 populations_count = 0
 populations = []
 
-
 def is_float(value):
     try:
         float(value)
         return True
     except:
         return False
-
 
 def default_values(n):
     global populations_count, param_values, coefficients, time, delta
@@ -48,37 +46,6 @@ def populations_from_matrix(matrix):
     for i in range(populations_count):
         populations.append(Population(i, *matrix[i]))
 
-
-#
-#
-# def read_from_file(filename):
-#     """Чтение данных из файла"""
-#     with open(filename, 'r') as file:
-#         first_line = True
-#         for line in file:
-#             if first_line:
-#                 n, t, dt = [int(x) for x in line.split()]
-#                 buff = [[0.0] * COLUMNS for i in range(n)]
-#                 first_line = False
-#                 i = 0
-#             else:
-#                 line = line.split()
-#                 for j in range(COLUMNS - 1):
-#                     buff[i][j] = float(line[j])
-#                 buff[i][COLUMNS - 1] = line[COLUMNS - 1]
-#                 i += 1
-#     global planets_count
-#     planets_count = n
-#     set_params(buff, t, dt, 2)
-#
-#
-# def write_to_file(filename):
-#     """Запись данных в файл"""
-#     with open(filename, 'w') as file:
-#         print(planets_count, time, time_step, file=file)
-#         for planet in planets:
-#             print(planet, file=file)
-
 class Population:
     def __init__(self, id: int, n: int, alpha: float):
         self.id = id
@@ -92,22 +59,19 @@ class Population:
 
     def interaction(self, objs, delta):
         f = 0
-        if self.N < 50 and not self.inRedBook:
-            self.alpha *= 2
+        if self.N < 30 and not self.inRedBook:
+            self.alpha *= 1.5
             self.inRedBook = True
-            print(self.id, 'in a red book')
-        elif self.N > 50 and self.inRedBook:
-            self.alpha /= 2
+        elif self.N > 30 and self.inRedBook:
+            self.alpha /= 1.5
             self.inRedBook = False
-            print(self.id, 'quit a red book')
         for obj in objs:
-            if self == obj:
-                continue
+            # if self == obj:
+            #     continue
             f += coefficients[self.id][obj.id] * self.N * obj.N
         self.N += delta * (self.N * self.alpha + f)
         if self.N < 2:
             self.N = 0
-
 
 def get_max_N():
     max = 0
--- Window.py
+++ Window.py
@@ -12,10 +12,12 @@ class Window:
         self.root = Tk()
         self.root.title(title)
         self.root.geometry(f"+100+75")
-        # self.root.resizable(False, False)
         self.displayed_time = StringVar(value="0")
         self.displayed_count = StringVar(value="0.0")
         self.button = self.fig = self.ax = self.canvas = None
+        self.button2 = None
+        self.a1 = StringVar(value="0")
+        self.a2 = StringVar(value="0")
 
     def run(self):
         self.draw()
@@ -40,20 +42,19 @@ class Window:
         Label(frame, text="Число особей (шт)").pack(pady=(10, 0))
         Entry(frame, width=25, textvariable=self.displayed_count, state=DISABLED).pack()
         self.button = Button(frame, width=20, text="Запуск модели", command=self.start_exec)
-        self.button.pack(pady=(20, 0))
-
-        # frame = Frame(self.root)
-        # frame.pack(side=LEFT, padx=12)
-        # self.button = Button(frame, width=20, height=10, text="Запуск модели", command=self.start_exec)
-        # self.button.pack()
-
+        self.button2 = Button(frame, width=20, text="Фазовые кривые", command=self.start_exec_phase)
+        self.button.pack(pady=(40, 0))
+        self.button2.pack(pady=(30, 0))
+        part_frame = Frame(frame)
+        part_frame.pack( pady=10)
+        Label(part_frame, text="индексы").pack(side=LEFT,padx=5)
+        Entry(part_frame, width=4, textvariable=self.a1).pack(side=LEFT,padx=5)
+        Entry(part_frame, width=4, textvariable=self.a2).pack(side=LEFT,padx=5)
     def draw_menu(self):
         main_menu = Menu(self.root)
 
         file_menu = Menu(main_menu, tearoff=0)
         file_menu.add_command(label="Новая система", command=self.create_system)
-        file_menu.add_command(label="Сохранить", command=self.save_file)
-        file_menu.add_command(label="Открыть", command=self.open_file)
         main_menu.add_cascade(label="Файл", menu=file_menu)
 
         main_menu.add_command(label="Параметры", command=self.change_params)
@@ -65,16 +66,6 @@ class Window:
     def change_params(self):
         a = Window2(self.root, "Параметры")
         a.draw()
-
-    def open_file(self):
-        filename = tkinter.filedialog.askopenfilename(filetypes=(("Text file", ".txt"),))
-        if filename:
-            Config.read_from_file(filename)
-
-    def save_file(self):
-        filename = tkinter.filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text file", "*.txt"),))
-        if filename:
-            Config.write_to_file(filename)
 
     def start_exec(self):
         print('\nStarted execution...')
@@ -91,7 +82,7 @@ class Window:
 
         def anim(frame):
             xdata.append(frame)
-            n=0
+            n = 0
             for i, population in enumerate(Config.populations):
                 population.interaction(Config.populations, Config.delta)
                 ydata[i].append(population.N)
@@ -104,6 +95,44 @@ class Window:
 
         xdata = []
         ydata = [[] for _ in range(Config.populations_count)]
+        x = np.arange(0, Config.time, Config.delta)
+        anim = animation.FuncAnimation(self.fig, func=anim, frames=x, interval=1,
+                                       blit=True, repeat=False)
+        self.canvas.draw()
+
+    def start_exec_phase(self):
+        print('\nStarted execution...')
+        print(Config.populations_count)
+        jo= int(self.a1.get())
+        pa = int(self.a2.get())
+        for i, population in zip(range(Config.populations_count), Config.populations):
+            print(i, population)
+        if Config.populations_count == 0:
+            tkinter.messagebox.showerror("Ошибка!", "Сначала необходимо создать систему")
+            return
+        self.ax.cla()
+        self.ax.set_xlim([0, 1.5 * Config.get_max_N()])
+        self.ax.set_ylim([0, 1.5 * Config.get_max_N()])
+
+        lines = [self.ax.plot([], [])[0] for _ in range(Config.populations_count)]
+
+        def anim(frame):
+            n=0
+            for i, population in enumerate(Config.populations):
+                population.interaction(Config.populations, Config.delta)
+                if i == jo:
+                    xdata.append(population.N)
+                if i == pa:
+                    ydata.append(population.N)
+                lines[0].set_data(xdata, ydata)
+                n += population.N
+
+            self.displayed_time.set(str(frame))
+            self.displayed_count.set(str(n))
+            return lines
+
+        xdata = []
+        ydata = []
         x = np.arange(0, Config.time, Config.delta)
         anim = animation.FuncAnimation(self.fig, func=anim, frames=x, interval=1,
                                        blit=True, repeat=False)
